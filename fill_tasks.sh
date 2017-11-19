30efd617b84f1db852387a9ec0db4be61fc6b562
Viktor Marchenko <viktor.marchenko@gmail.com>
2017-11-19 23:08:16 +0300

    Do not add tomorrow line on Sundays

--- fill_tasks.sh
+++ fill_tasks.sh
@@ -7,7 +7,12 @@ CURRENT_MONDAY_COMMAND="date -v-$(($(date +%u)-1))d"
 CURRENT_FILE="../_Done_$($CURRENT_MONDAY_COMMAND +%Y-%m-%d_W%V).md"
 TODAY_FILE="$(date +%d.%m.%Y).txt"
 
+cat $TODAY_FILE >> $CURRENT_FILE
+
+
+numdaycheck="$(date +%u)"
 TOMORROW_COMMAND="date -v+1d"
 
-cat $TODAY_FILE >> $CURRENT_FILE
-echo "\n\n#### $($TOMORROW_COMMAND "+%A, %B %d")" >> $CURRENT_FILE
+if [ $numdaycheck != 7 ]; then
+  echo "\n\n#### $($TOMORROW_COMMAND "+%A, %B %d")" >> $CURRENT_FILE
+fi
