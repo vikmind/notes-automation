c0ee30a81cff09f8898f6e95f84f8deb8ff2422f
Viktor Marchenko <viktor.marchenko@gmail.com>
2019-07-23 19:16:34 +0300

    Add escape sequences for echo script

--- show_focus.sh
+++ show_focus.sh
@@ -4,16 +4,16 @@ RED='\033[1;31m'
 NC='\033[0m'
 DIR="$(dirname $0)/.."
 
-echo "\n${RED}FOCUS ON:${NC}\n"
+echo -e "\n${RED}FOCUS ON:${NC}\n"
 COUNT=0
 while read line
 do
   COUNT=$(($COUNT+1))
   DIV3=$(($COUNT % 3))
   if [ $DIV3 == 1 ]; then
-    echo "$GREEN$line$NC"
+    echo -e "$GREEN$line$NC"
   else
-    echo "$line"
+    echo -e "$line"
   fi
 done < $DIR/focus.txt
 
