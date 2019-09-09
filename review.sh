a5d5352f0248c76719bb11472498d3cf6562a813
Viktor Marchenko <viktor.marchenko@gmail.com>
2019-09-09 23:12:24 +0300

    Add todoist sync before daily review

--- review.sh
+++ review.sh
@@ -5,6 +5,8 @@ CURRENT_MONDAY_COMMAND="date -v-$(($(date +%u)-1))d"
 CURRENT_FILE="$DIR/_Done_$($CURRENT_MONDAY_COMMAND +%Y-%m-%d_W%V).md"
 TODAY_FILTER="$(date +%y/%m/%d)"
 
+todoist s
+
 TODAY_TASKS=$(todoist --csv cl | rg "$TODAY_FILTER" | rg -e '^\d.+#"?([^"]+)"?,"?([^"]+)"?$' -r '- [$1] $2\n' | tr -d '\n')
 
 echo $TODAY_TASKS >> $CURRENT_FILE
