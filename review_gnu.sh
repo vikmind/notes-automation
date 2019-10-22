65bc78b68a0da784102225418f8253279fcff3b8
Viktor Marchenko <viktor.marchenko@gmail.com>
2019-10-22 23:01:56 +0300

    Separate script for review daily tasks

---
+++ review_gnu.sh
@@ -0,0 +1,19 @@
+DIR="$( cd "$( dirname "$0" )/../" && pwd )"
+
+CURRENT_MONDAY_COMMAND="date -d'next monday - 7 days'"
+$CURRENT_MONDAY_COMMAND
+CURRENT_FILE="$DIR/_Done_$($CURRENT_MONDAY_COMMAND +%Y-%m-%d_W%V).md"
+TODAY_FILTER="$(date +%y/%m/%d)"
+
+TODAY_TASKS=$(todoist --csv cl | rg "$TODAY_FILTER" | rg -e '^\d.+#"?([^"]+)"?,"?([^"]+)"?$' -r '- [$1] $2\n' | tr -d '\n')
+
+echo $TODAY_TASKS >> $CURRENT_FILE
+
+numdaycheck="$(date +%u)"
+TOMORROW_COMMAND="date -dtomorrow"
+
+if [ $numdaycheck != 7 ]; then
+  echo "#### $($TOMORROW_COMMAND "+%A, %B %d")" >> $CURRENT_FILE
+fi
+
+$EDITOR $CURRENT_FILE
